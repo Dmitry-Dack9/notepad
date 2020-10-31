018ed0b4a97b0ffc3b75a109a642f37557fd7d52
Dmitry-Dack9 <slipin2020@mail.ru>
2020-10-31 18:49:00 +0300

    Add note.rb

--- lib/post.rb
+++ lib/post.rb
@@ -5,27 +5,33 @@ class Post
   end
 
   def read_from_console
-
   end
 
   def to_strings
-
   end
 
   def save
     file = File.new(file_path, "w:UTF-8")
 
-    for item in string do
+    for item in to_strings do
       file.puts(item)
     end
     file.close
   end
 
   def file_path
-    current_path = __dir_
+    current_path = __dir__
 
-    file_name = @created_at.strftime("#{self.class.name}_%Y-%m-%d_%H-%m-%s.txt")
+    file_name = @created_at.strftime("#{self.class.name}_%Y-%m-%d_%H-%M-%S.txt")
 
     return File.join(current_path, file_name)
+  end
+
+  def self.post_types
+    [Memo, Link, Task]
+  end
+
+  def self.create(type_index)
+    return post_types[type_index].new
   end
 end
